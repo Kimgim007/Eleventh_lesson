a4a648a1939a410bf8d716fa6d4028d607c81094
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-04-30 14:40:54 +0300

    Fix2

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eleventh_lesson
+{
+    public class Car<T> where T : AbstractEngine
+    {
+        public Car() { }
+        public Car(T engine)
+        {
+            this.Engine = engine;
+        }
+
+        public T Engine { get; set; }
+
+        public void StartCar()
+        {
+            Console.WriteLine($"Машина едет");
+        }
+
+        public void StopCar()
+        {
+            Console.WriteLine("Машина остановилась");
+        }
+
+    }
+}
